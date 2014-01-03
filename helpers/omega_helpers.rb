dd64e1db6fa7feebf7593b3e5209d55b5b5374d4
Mo Morsi <mmorsi@redhat.com>
2014-01-03 12:32:32 -0500

    Add a couple more helpers, link the blog main page from the logo img

--- helpers/omega_helpers.rb
+++ helpers/omega_helpers.rb
@@ -10,7 +10,11 @@ module OmegaHelpers
     link_to '<< back', '/', :class => 'back_link'
   end
 
-  def project_link(text, dir)
+  def rjr_link(text)
+    link_to text, "http://github.com/movitto/rjr"
+  end
+
+  def project_link(text, dir='')
     link_to text, "http://github.com/movitto/omega/tree/master/#{dir}"
   end
 
@@ -20,6 +24,14 @@ module OmegaHelpers
 
   def commit_link(text, commit)
     link_to text, "http://github.com/movitto/omega/commit/" + commit
+  end
+
+  def post_link(text, post_id)
+    link_to text, "/posts/#{post_id}.html"
+  end
+
+  def wiki_link(text, page)
+    link_to text, "http://github.com/movitto/omega/wiki/#{page}"
   end
 
   def screenshot_thumb(id, opts={})
